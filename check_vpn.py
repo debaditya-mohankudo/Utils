22bc51d08c3e9ac74932d37f610052ac17a2cfb5
debaditya mohankudo <debaditya.mohankudo@digicert.com>
2022-09-19 12:34:24 +0530

    handle exception

--- check_vpn.py
+++ check_vpn.py
@@ -2,11 +2,12 @@ import socket
 import time
 
 while True:
-	IPaddress=socket.gethostbyname("repo.dev.tgp.digicert.com")
-	if IPaddress=="172.16.32.100":
-		print("Connected, with the url: "+ "repo.dev.tgp.digicert.com"  + '-' + time.ctime())
-	else:
-		print("No vpn")
+	try:
+		IPaddress=socket.gethostbyname("repo.dev.tgp.digicert.com")
+		if IPaddress=="172.16.32.100":
+			print("Connected, with the url: "+ "repo.dev.tgp.digicert.com"  + '-' + time.ctime())
+	except socket.gaierror:
+		"No connection"
 	
 	time.sleep(15)
 	print(int(time.time() % 100) * '-')

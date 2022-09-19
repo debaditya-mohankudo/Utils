86f5c8ca10d7026113439b5c411a5f8531281ec9
debaditya mohankudo <debaditya.mohankudo@digicert.com>
2022-09-19 10:42:59 +0530

    check vpn

--- check_vpn.py
+++ check_vpn.py
@@ -4,9 +4,9 @@ import time
 while True:
 	IPaddress=socket.gethostbyname("repo.dev.tgp.digicert.com")
 	if IPaddress=="172.16.32.100":
-		print("Connected, with the IP address: "+ "repo.dev.tgp.digicert.com"  + '-' + time.ctime())
+		print("Connected, with the url: "+ "repo.dev.tgp.digicert.com"  + '-' + time.ctime())
 	else:
-		print("No internet, your localhost is "+ "repo.dev.tgp.digicert.com" + '-' + time.ctime())
+		print("No vpn")
 	
 	time.sleep(15)
 	print(int(time.time() % 100) * '-')

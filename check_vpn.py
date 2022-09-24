32bb063ecc9a258c1c9319fc274854ae1f32a3a8
debaditya <debaditya.mohankudo@digicert.com>
2022-09-25 05:16:10 +0530

    more formatting

--- check_vpn.py
+++ check_vpn.py
@@ -1,11 +1,13 @@
 import socket
 import time
-from tqdm import tqdm
+from tqdm import tqdm # pip install tqdm
 
 while True:
 	try:
 		IPaddress=socket.gethostbyname("repo.dev.tgp.digicert.com")
+		print("")
 		print("Connected, with the url: "+ "repo.dev.tgp.digicert.com"  + '-' + time.ctime())
+		print("")
 		for i in tqdm (range (15), desc="Sleeping..."):
 			time.sleep(1)
 

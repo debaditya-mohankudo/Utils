6be45ace95dee41040394f9d40b74ab2624da5a8
debaditya <debaditya.mohankudo@digicert.com>
2022-09-25 16:00:37 +0530

    latest

--- check_vpn.py
+++ check_vpn.py
@@ -2,14 +2,16 @@ import socket
 import time
 from tqdm import tqdm # pip install tqdm
 
+start = time.time()
 while True:
 	try:
 		IPaddress=socket.gethostbyname("repo.dev.tgp.digicert.com")
 		print("")
-		print("Connected, with the url: "+ "repo.dev.tgp.digicert.com"  + '-' + time.ctime())
+		print("Connected to : "+ "repo.dev.tgp.digicert.com"  + ' For ' + str(time.time() - start) + ' Seconds')
 		print("")
 		for i in tqdm (range (15), desc="Sleeping..."):
 			time.sleep(1)
 
 	except socket.gaierror:
 		print("No connection")
+		time.sleep(15)

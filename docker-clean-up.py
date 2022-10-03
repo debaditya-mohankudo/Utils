41cbc6070eaf7b8b2c5794686dc962b1d8be103d
debaditya M <debaditya.mohankudo@digicert.com>
2022-10-04 05:28:17 +0530

    use argparse

--- docker-clean-up.py
+++ docker-clean-up.py
@@ -1,36 +1,63 @@
 from python_on_whales import docker
 
-# pause all containers
-for container in docker.container.list(all=True):
-    if container.state.status == 'running':
-        docker.pause(container)
+import argparse
 
-# stop all containers
-for container in docker.container.list(all=True):
-    print('stopping...', container.name)
-    container.stop()
-    container.remove()
-docker.container.prune()
+parser = argparse.ArgumentParser(description='Docker clean up')
+parser.add_argument('--operation', type=str, help='valid values are : clean/pause/unpause')
 
-# remove all networks
-for network in docker.network.list():
-    if network.name in ('none', 'bridge', 'host'):
-        pass
-    else:
-        print('network removed', network.name)
-        network.remove()
-docker.network.prune()
 
-# remove all volumes
-for volume in docker.volume.list():
-    print('remove volume', volume.name)
-    volume.remove()
-docker.volume.prune()
 
-# remove all images with None tag
-for image in docker.image.list():
-    if image.repo_tags == []:
-        print(image.repo_tags, image.size // (1024 * 1024), 'MB')
-        print('removing image with no tags')
-        image.remove(prune=True)
-   
+args = parser.parse_args()
+print(args.operation)
+
+
+
+
+if args.operation == 'pause':
+    print('pause all containers')
+    for container in docker.container.list(all=True):
+        if container.state.status == 'running':
+            docker.pause(container)
+
+if args.operation == 'unpause':
+    print('unpause all containers')
+    # pause all containers
+    for container in docker.container.list(all=True):
+        if container.state.status == 'paused':
+            docker.unpause(container)
+
+if args.operation == 'clean':
+    # pause all containers
+    for container in docker.container.list(all=True):
+        if container.state.status == 'running':
+            docker.pause(container)
+
+    # stop all containers
+    for container in docker.container.list(all=True):
+        print('stopping...', container.name)
+        container.stop()
+        container.remove()
+    docker.container.prune()
+
+    # remove all networks
+    for network in docker.network.list():
+        if network.name in ('none', 'bridge', 'host'):
+            pass
+        else:
+            print('network removed', network.name)
+            network.remove()
+    docker.network.prune()
+
+    # remove all volumes
+    for volume in docker.volume.list():
+        print('remove volume', volume.name)
+        volume.remove()
+    docker.volume.prune()
+
+    # remove all images with None tag
+    for image in docker.image.list():
+        if image.repo_tags == []:
+            print(image.repo_tags, image.size // (1024 * 1024), 'MB')
+            print('removing image with no tags')
+            image.remove(prune=True)
+    

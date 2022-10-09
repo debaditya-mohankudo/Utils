e342fe52fe8d9866e08d2d62b0970939e1cca29a
debaditya M <debaditya.mohankudo@digicert.com>
2022-10-09 16:48:27 +0530

    Accepts a list of container names and follows it

--- get_logs_for_containers_journald.py
+++ get_logs_for_containers_journald.py
@@ -1,18 +1,7 @@
+
+'''Usage
+python ~/workspace/Utils/get_logs_for_containers_journald.py  container_name1 contianer_name2 ...  
 '''
-Usage: get_logs_for_containers_journald.py [OPTIONS]
-
-Options:
-  --container-name TEXT
-  --install-completion [bash|zsh|fish|powershell|pwsh]
-                                  Install completion for the specified shell.
-  --show-completion [bash|zsh|fish|powershell|pwsh]
-                                  Show completion for the specified shell, to
-                                  copy it or customize the installation.
-  --help                          Show this message and exit.
-
-'''
-
-
 import json
 import typer
 import time
@@ -23,7 +12,7 @@ from colorama import Fore
 from colorama import Style
 from psutil import Popen
 from subprocess import PIPE
-from typing import Optional
+from typing import Optional, List
 
 
 
@@ -33,13 +22,13 @@ print(f"This is best viewd in {Fore.WHITE} BLACK COLOR BACKGROUND TERMINAL{Style
 app = typer.Typer()
 
 @app.command()
-def follow_logs(container_name: Optional[str] = None) -> None:
-    tail_logs_from_container(container_name)
+def follow_logs(cnames: List[str]) -> None:
+    tail_logs_from_container(cnames)
 
 def print_in_color(color: Fore, text: str) -> None:
     print(f"{color} {text} {Style.RESET_ALL}")
 
-def print_logs_from_stream(logs_stream: Popen, container_name: Optional[str]) -> None:
+def print_logs_from_stream(logs_stream: Popen) -> None:
     color = Fore.WHITE
     while True:
         log = json.loads(logs_stream.stdout.readline())
@@ -56,7 +45,7 @@ def print_logs_from_stream(logs_stream: Popen, container_name: Optional[str]) ->
         else:
             time.sleep(1)
 
-def tail_logs_from_container(container_name: Optional[str] = None) -> None:
+def tail_logs_from_container(container_names: List[str]) -> None:
     command = [ "journalctl", 
         "--since", 
         "5 seconds ago", 
@@ -69,15 +58,14 @@ def tail_logs_from_container(container_name: Optional[str] = None) -> None:
         #"-n", "1"
     ]
 
-    if container_name:
-        command.append( f"CONTAINER_NAME={container_name}")
+    for c in container_names:
+        command.append( f"CONTAINER_NAME={c}")
 
     print(f"Running command: {command}")
     
     docker_logs_stream = Popen(command, stdout=PIPE)
-    print(type(docker_logs_stream))
 
-    print_logs_from_stream(docker_logs_stream, container_name)
+    print_logs_from_stream(docker_logs_stream)
 
 
 if __name__ == "__main__":

cea8119163d70473da59b414b49636018a14d05a
debaditya M <debaditya.mohankudo@digicert.com>
2022-10-10 20:10:17 +0530

    use typer

--- check_vpn.py
+++ check_vpn.py
@@ -1,25 +1,29 @@
+
 import socket
 import time
+import typer
 
 from rich.progress import track # pip install rich
 from rich import print
 
 duration_to_check_sec = 30
 
-start = time.time()
-while True:
-	try:
-		print("\033c", end="") # to clear the terminal using python (edited) 
-		IPaddress=socket.gethostbyname("repo.dev.tgp.digicert.com")
-		print("")
-		print(f"=<>= Connected to : repo.dev.tgp.digicert.com For  {int((time.time() - start))}s")
-		print("")
-		for i in track(range (duration_to_check_sec), description="Sleeping..."):
-			time.sleep(1)
+def test_connection(site: str) -> None: 
+	start = time.time()
+	while True:
+		try:
+			print("\033c", end="") # to clear the terminal using python (edited) 
+			IPaddress=socket.gethostbyname(site)
+			print(f"=<>= Connected to : {site} For  {int((time.time() - start))}s")
+			for i in track(range (duration_to_check_sec), description="Sleeping..."):
+				time.sleep(1)
 
-	except socket.gaierror:
-		print("\033c", end="") # to clear the terminal using python (edited) 
-		start = time.time()
-		print("No connection")
-		time.sleep(duration_to_check_sec)
-		print(f"=><= Disconnected from : repo.dev.tgp.digicert.com For {(time.time() - start)}s")
+		except socket.gaierror:
+			print("\033c", end="") # to clear the terminal using python (edited) 
+			print("No connection")
+			time.sleep(duration_to_check_sec)
+			start = time.time()
+			print(f"=><= Disconnected from : {site}")
+
+if __name__ == "__main__":
+	typer.run(test_connection)
